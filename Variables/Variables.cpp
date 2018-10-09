64cf5e689c476ac1042ed83d71207dcb52e7c4bc
coderguy1777 <hill.jordan@charterschool.org>
2018-10-08 21:59:36 -0400

    Variables Practice with C++

---
+++ Variables/Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	double a; //Ints in C++
+	double b,c,d; //Doubles in C++
+	a = 10;
+	b = 12.35;
+	c = a - b;
+	cout << c << endl;
+	system("pause");
+	return 0;
+	
+}
